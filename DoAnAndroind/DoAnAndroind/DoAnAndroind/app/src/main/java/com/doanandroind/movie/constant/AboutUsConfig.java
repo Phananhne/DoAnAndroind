ef700e5dc0951d8c28abe5069ad4a1ad9ae2c8eb
Phananhne <132130146+Phananhne@users.noreply.github.com>
2024-04-15 19:37:45 +0700

    add

---
+++ DoAnAndroind/DoAnAndroind/DoAnAndroind/app/src/main/java/com/doanandroind/movie/constant/AboutUsConfig.java
@@ -0,0 +1,6 @@
+package com.doanandroind.movie.constant;
+
+public interface AboutUsConfig {
+    String ABOUT_US_TITLE = "MovieHub";
+    String ABOUT_US_SLOGAN = "Khám phá thế giới phim ngay bây giờ";
+}
